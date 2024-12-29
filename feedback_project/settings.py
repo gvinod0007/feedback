3df39640ae8ef7786eb6c845e46fc5b8d76401c1
gvinod0007 <gvinod0007@gmail.com>
2024-12-29 18:12:18 +0530

    new

--- feedback_project/settings.py
+++ feedback_project/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ALLOWED_HOSTS = ['*']
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -74,12 +76,12 @@ WSGI_APPLICATION = 'feedback_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -125,13 +127,13 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sample',
-        'USER': 'postgres',
-        'PASSWORD': 'Vinod',
-        'HOST': 'postgres-db',  # Service name in docker-compose.yml
-        'PORT': '5432',         # Internal PostgreSQL port
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sample',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Vinod',
+#         'HOST': 'postgres-db',  # Service name in docker-compose.yml
+#         'PORT': '5432',         # Internal PostgreSQL port
+#     }
+# }
